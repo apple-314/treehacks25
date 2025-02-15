b14c4a53a75c04a1f7f786ad62cc113a291453ee
AaravWattal <aaravwattal@gmail.com>
2025-02-15 00:02:04 -0800

    add streaming code

---
+++ VisionProTest021325App.swift
@@ -0,0 +1,23 @@
+//
+//  VisionProTest021325App.swift
+//  VisionProTest021325
+//
+//  Created by Aarav Wattal on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionProTest021325App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+//    
+//    var body: some RealityKit.Scene {
+//        WindowGroup {
+//            ContentView()
+//        }.windowStyle(.plain)
+//    }
+}
